9f76fc07ae44708a8df2b6b4038bca5f36f3750d
rogermelich <rogermelich@gmail.com>
2015-11-10 19:20:56 +0100

    Create main.js and create var hello

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,9 @@
 /**
  * Created by roger on 10/11/15.
  */
-$("#hello").html('helloworld');
-alert("Helloworld2")
+//$("#hello").html('helloworld');
+//alert("Helloworld2");
+
+var hello = require('./hello');
+
+hello();
