e1fee4da3b2f9705edc6091be03338dbefb817d4
rogermelich <rogermelich@gmail.com>
2015-11-10 20:19:17 +0100

    Create Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,32 @@
-var elixir = require('laravel-elixir');
+//var elixir = require('laravel-elixir');
+//
+///*
+// |--------------------------------------------------------------------------
+// | Elixir Asset Management
+// |--------------------------------------------------------------------------
+// |
+// | Elixir provides a clean, fluent API for defining some basic Gulp tasks
+// | for your Laravel application. By default, we are compiling the Sass
+// | file for our application, as well as publishing vendor resources.
+// |
+// */
+//
+//elixir(function(mix) {
+//    mix.sass('app.scss');
+//});
 
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Sass
- | file for our application, as well as publishing vendor resources.
- |
- */
+var gulp = require('gulp');
+var browserify = require('glup-browserify');
+var rename = require('gulp-rename');
 
-elixir(function(mix) {
-    mix.sass('app.scss');
+gulp.task('js', function(){
+ gulp.src ('public/js/main.js')
+     .pipe(browserify())
+     .pipe(rename('public/js/bundle.js'))
+     .pipe(gulp.dest('./'));
 });
+
+gulp.task('watch', function(){
+ gulp.watch('public/js/**/*.js')
+});
+
