60f2b6ef1972c8ce2a2beb17c56052e81997134e
rogermelich <rogermelich@gmail.com>
2015-11-10 18:29:57 +0100

    modified welcome.blade.php and include jquery

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,44 +2,15 @@
 <html>
     <head>
         <title>Laravel</title>
-
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-        </style>
     </head>
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div id="hello" class="title">Hola</div>
             </div>
         </div>
+        <script src="https://code.jquery.com/jquery-2.1.4.js"></script>
+        <script src="js/main.js"></script>
     </body>
 </html>
